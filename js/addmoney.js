be786b97bec3386e6e03fa0348230e22a4c7ebad
MonyeemTAMZID <abdulmonyeemtamzid@gmail.com>
2024-09-25 01:20:12 +0600

    done with all challenges only history left

--- js/addmoney.js
+++ js/addmoney.js
@@ -5,16 +5,42 @@ document.getElementById('btn-donate-noakhali').addEventListener('click', functio
 
     const addMoneyN = getInputFieldByValue('noakhali-add-money');
     const increaseBalanceN = getTextFieldValueById('noakhali-balance');
+    // minus from total balance
+    const totalBalance = getTextFieldValueById('balance');
+     // check if money donated is more than balance:
+     if(addMoneyN > totalBalance){
+        alert('not enough money!!');
+        return;
+    }
+
+    
+
+    const balancePostDonation = totalBalance - addMoneyN;
+
+    document.getElementById('balance').innerText = balancePostDonation;
+    
+    
+
     const newNoakhaliBalance = addMoneyN + increaseBalanceN;
 
     document.getElementById('noakhali-balance').innerText = newNoakhaliBalance +' BDT';
 
-    // minus from total balance
-    const totalBalance = getTextFieldValueById('balance');
-    const balancePostDonation = totalBalance - addMoneyN;
+    
 
-    document.getElementById('balance').innerText = balancePostDonation;
+    // check if input is integer:
+    if(isNaN(addMoneyN)){
+        alert('failed to Donate money!!');
+        return;
+    }
 
+    // pop-up:
+    
+
+    
+
+   
+
+    
 })
 
 //add money for feni:
@@ -24,16 +50,27 @@ document.getElementById('btn-donate-feni').addEventListener('click', function(ev
 
     const addMoneyF = getInputFieldByValue('feni-add-money');
     const increaseBalanceF = getTextFieldValueById('feni-balance');
-    const newFeniBalance = addMoneyF + increaseBalanceF;
-
-    document.getElementById('feni-balance').innerText = newFeniBalance +' BDT';
-
     // minus from total balance
     const totalBalance = getTextFieldValueById('balance');
+    // check if money donated is more than balance:
+    if(addMoneyF > totalBalance){
+        alert('not enough money!!');
+        return;
+    }
     const balancePostDonation = totalBalance - addMoneyF;
 
     document.getElementById('balance').innerText = balancePostDonation;
 
+    const newFeniBalance = addMoneyF + increaseBalanceF;
+
+    document.getElementById('feni-balance').innerText = newFeniBalance +' BDT';
+
+    if(isNaN(addMoneyF)){
+        alert('failed to add money!!');
+        return;
+    }
+
+    
 })
 
 // add money for qouta protest:
@@ -43,23 +80,30 @@ document.getElementById('btn-donate-qouta-protest').addEventListener('click', fu
 
     const addMoneyQ = getInputFieldByValue('quota-protest-add-money');
     const increaseBalanceQ = getTextFieldValueById('quota-protest-balance');
-    const newQoutaBalance = addMoneyQ + increaseBalanceQ;
-
-    document.getElementById('quota-protest-balance').innerText = newQoutaBalance +' BDT';
-
     // minus from total balance
     const totalBalance = getTextFieldValueById('balance');
+
+    if(addMoneyQ > totalBalance){
+        
+        alert('not enough money!!');
+        document.getElementById('quota-protest-balance').innerText = '0 BDT';
+        return;
+    }
+
+    
     const balancePostDonation = totalBalance - addMoneyQ;
 
     document.getElementById('balance').innerText = balancePostDonation;
 
+    const newQoutaBalance = addMoneyQ + increaseBalanceQ;
+
+    document.getElementById('quota-protest-balance').innerText = newQoutaBalance +' BDT';
+
+    if(isNaN(addMoneyQ  )){
+        alert('failed to add money!!');
+        return;
+    }
+
+    
 })
 
-// transaction history:
-const div = document.createElement('div');
-        div.classList.add('bg-gray-200');
-        div.innerHTML = `
-        
-        <h3 class = 'font-bold' >${addMoneyN} Taka is donated for relief for NoaKhali,Bangladesh. </h3>
-        `
-        document.getElementById('transaction container').appendChild(div);
--- js/features.js
+++ js/features.js
@@ -3,16 +3,27 @@ document.getElementById('btn-donate').addEventListener('click', function(){
     // remove hidden class from donation form:
     document.getElementById('donation-form').classList.remove('hidden');
 
+    // color change of the button clicked:
+    document.getElementById('btn-donate-history').classList.remove('bg-[#B4F461]');
+    document.getElementById('btn-donate').classList.add('bg-[#B4F461]');
+
     // hide the donation history form:
-    document.getElementById('transaction-section').classList.add('hidden');
+    document.getElementById('donation-section').classList.add('hidden');
 })
 
 // show the donation history form:
 document.getElementById('btn-donate-history').addEventListener('click', function(){
     // remove hidden class from donation history:
-    document.getElementById('transaction-section').classList.remove('hidden');
+    document.getElementById('donation-section').classList.remove('hidden');
+
+    // color change of the button clicked:
+    document.getElementById('btn-donate-history').classList.add('bg-[#B4F461]');
+    document.getElementById('btn-donate').classList.remove('bg-[#B4F461]');
 
     // hide the donation form:
     document.getElementById('donation-form').classList.add('hidden');
+
+    // hide the footer:
+    document.getElementById('footer').classList.add('hidden');
 
 })
